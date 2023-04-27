8b0ef29d2fae112e1b0f879064a0e6e5c571e065
lucas <lucas.escaffre@ynov.com>
2023-04-27 23:53:25 +0200

    add: when the instance is created, an express server and nginx server starts on the instance with terraform

--- install-website.sh
+++ install-website.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-sudo apt install nginx -y &&
+sudo su -
+apt install nginx -y &&
 systemctl start nginx &&
 systemctl enable nginx &&
 ufw allow http &&
-ufw allow https
+ufw allow https &&
+
+curl -fsSL https://get.docker.com -o get-docker.sh &&
+sh get-docker.sh &&
+
+systemctl start docker &&
+
+ufw enable &&
+ufw allow 3000/tcp &&
+ufw allow 8080/tcp &&
+
+touch /etc/nginx/sites-available/projet-si.conf &&
+
+echo "server {
+    listen 80;
+    server_name projet-si.lucesf.com;
+    location / {
+        proxy_pass http://localhost:3000;
+    }
+}" >> /etc/nginx/sites-available/projet-si.conf &&
+
+docker run -d -p 3000:3000 lucesf/projet-si
